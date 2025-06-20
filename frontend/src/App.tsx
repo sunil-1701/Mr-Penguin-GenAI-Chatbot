6e26864947aa1c7c4a101e9c3f6d7585557b915a
Sunil Kushwaha <128570469+sunil-1701@users.noreply.github.com>
2025-06-20 20:08:11 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -7,18 +7,30 @@ import Chat from "./pages/Chat";
 import NotFound from "./pages/NotFound";
 import { useAuth } from "./context/AuthContext";
 function App() {
-
-  const auth =useAuth();
-  return <main>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      {auth?.isLoggedIn && auth.user && <Route path="/chat" element={<Chat/>}/>}
-      <Route path="*" element={<NotFound/>}/>
-    </Routes>
-  </main>;
+  const auth = useAuth();
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="https://mr-penguin.onrender.com/" element={<Home />} />
+        <Route
+          path="https://mr-penguin.onrender.com/login"
+          element={<Login />}
+        />
+        <Route
+          path="https://mr-penguin.onrender.com/signup"
+          element={<Signup />}
+        />
+        {auth?.isLoggedIn && auth.user && (
+          <Route
+            path="https://mr-penguin.onrender.com/chat"
+            element={<Chat />}
+          />
+        )}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+export default App;
